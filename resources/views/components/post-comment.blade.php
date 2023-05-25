cde1be50c793d8b938da6a59cb0860bb83272d06
Fabian <github@fabian-hof.de>
2023-05-25 14:47:24 +0200

    ...

---
+++ resources/views/components/post-comment.blade.php
@@ -0,0 +1,17 @@
+<article class="flex bg-gray-100 p-6 border border-gray-200 rounded-xl space-x-4">
+    <div class="flex-shrink-0">
+        <img src="https://i.pravatar.cc/60" alt="" class="rounded-xl" width="60" height="60">
+    </div>
+
+    <div>
+        <header class="mb-4">
+            <h3 class="font-bold">Martina Mustermann</h3>
+            <p class="text-xs"><time>Vor 8 Monaten</time></p>
+
+        </header>
+
+        <p>
+            Bliblablubb und Trallala, Heißahopp und hoppsassa
+        </p>
+    </div>
+</article>
